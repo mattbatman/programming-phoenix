e5e2b2e6fefb8e8ce9e53f608b0214a5a95152d3
Matt Batman <batman.matt@gmail.com>
2021-08-31 11:29:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programming Phoenix 1.4
+
+This is my campanion code typed while reading the book [Programming Phoenix 1.4](https://pragprog.com/titles/phoenix14/programming-phoenix-1-4/).
---
+++ config/test.exs
@@ -0,0 +1,29 @@
+use Mix.Config
+
+config :info_sys, :wolfram,
+  app_id: "1234",
+  http_client: InfoSys.Test.HTTPClient
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :rumbl, Rumbl.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "rumbl_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :rumbl_web, RumblWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# reduce hashing rounds in the test env to speed up tests
+config :pbkdf2_elixir, :rounds, 1
